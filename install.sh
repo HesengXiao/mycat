3432562ce3a5dcb0ac546ea9db7a8a7267fb36c5
HesengXiao <51151814+HesengXiao@users.noreply.github.com>
2024-04-14 03:16:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -ue
-<<<<<<< HEAD
-
 MYCAT_DIR=/usr/local/mycat
 MYCAT_CONF=${MYCAT_DIR}/conf 
 MYCAT_LOGS=${MYCAT_DIR}/logs
@@ -11,5 +9,3 @@ MYCAT_MAMAGER_PORT=9066
 
 MYCAT_PKG_NAME="Mycat-server-1.6.7.4-release-20200105164103-linux.tar.gz"
 MYCAT2_PGK=(   )
-=======
->>>>>>> 793011deffd84a391b0843ad6855106dfdcd7d66
