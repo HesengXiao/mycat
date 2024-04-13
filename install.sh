fd1bc3257c984cf794753a0b4eaa96b71eb87a5e
HesengXiao <761708923@qq.com>
2024-04-14 02:49:16 +0800

    update

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ue
+
+MYCAT_DIR=/usr/local/mycat
+MYCAT_CONF=${MYCAT_DIR}/conf 
+MYCAT_LOGS=${MYCAT_DIR}/logs
+
+MYCAT_CLIENT_PROT=8066
+MYCAT_MAMAGER_PORT=9066
+
+MYCAT_PKG_NAME="Mycat-server-1.6.7.4-release-20200105164103-linux.tar.gz"
+MYCAT2_PGK=(   )
